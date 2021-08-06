dd7a787c7df8872db3e34bef7c006073c8ff9c63
jbeecy <john.e.beecy@gmail.com>
2021-08-06 12:15:14 -0400

    add working router functions for all post CRUD
    This commit adds all api and router work for the user posts.
    All curl scripts work correctly in the terminal.

--- app/routes/user_post_routes.js
+++ app/routes/user_post_routes.js
@@ -36,12 +36,41 @@ router.get('/posts', requireToken, (req, res, next) => {
 
 // SHOW
 // GET/ posts/id
+router.get('/posts/:id', requireToken, (req, res, next) => {
+  // below statement will be based on the id in the route of the request
+  Post.findById(req.params.id)
+    // need to handle 404 which is no content
+    .then(handle404)
+    .then(post => res.status(200).json({ post: post.toObject() }))
+    .catch(next)
+})
 
 // UPDATE
 // PATCH/ posts/id
+router.patch('/posts/:id', requireToken, (req, res, next) => {
+  delete req.body.post.owner
+  Post.findById(req.params.id)
+    .then(handle404)
+    .then(post => {
+      requireOwnership(req, post)
+      return post.updateOne(req.body.post)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
 
 // DESTROY
 // DELETE /posts/id
+router.delete('/posts/:id', requireToken, (req, res, next) => {
+  Post.findById(req.params.id)
+    .then(handle404)
+    .then(post => {
+      requireOwnership(req, post)
+      post.deleteOne()
+        .then(() => res.sendStatus(204))
+        .catch(next)
+    })
+})
 
 // module exports the router
 module.exports = router
